e41ecb71a430b190c6e82fba18afaad37d7e1c5b
darshanaw <40012508+darshanaw@users.noreply.github.com>
2018-04-23 07:06:59 +0000

    Partially implemented Password changing option.

---
+++ HonanClaimsWebApi/Models/PasswordResetModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonanClaimsWebApi.Models
+{
+    public class PasswordResetModel
+    {
+        [Required]
+        [Display(Name = "User Code")]
+        public string UserCode { get; set; }
+
+
+        public string UserId { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        [RegularExpression(@"^(?=.*\d).{8,}$", ErrorMessage ="Password must be minimum 8 characters with atleast 1 number")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
+
+
+        public int DaysLeft { get; set; }
+    }
+}
