beb7a329459ee0f395e92aec8923da09f975f8b4
darshanaw <40012508+darshanaw@users.noreply.github.com>
2018-04-27 03:20:39 +0000

    Updated the File Notes to be same as previous

--- HonanClaimsWebApi/Models/Claim/FileNote.cs
+++ HonanClaimsWebApi/Models/Claim/FileNote.cs
@@ -12,7 +12,7 @@ namespace HonanClaimsWebApi.Models.Claim
         public string H_ClaimsId { get; set; }
         public string CreatedUserId { get; set; }
         public string CreatedUser { get; set; }
-        public string CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string ShortDescription { get; set; }
         public DateTime FileNoteDate { get; set; }
         public string Details { get; set; }
